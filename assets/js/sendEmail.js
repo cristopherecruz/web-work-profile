e162ad7dfa0159792ab543d79a7178b5412e621d
ceduardoc <cristopher.ecruz@gmail.com>
2022-11-11 19:45:26 -0300

    send email

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,21 @@
+let form = document.querySelector(".principal__form");
+let botaoSubmit = document.querySelector(".enviar");
+let personalName = document.querySelector("#nome-sobrenome");
+let email = document.querySelector("#email");
+let phoneNumber = document.querySelector("#telefone");
+let message = document.querySelector("#mensagem");
+
+botaoSubmit.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    Email.send({
+        SecureToken : "974459ee-c235-4b12-863b-d25c14452813",
+        To : 'cristopher@cristopher.dev.br',
+        From : `${email.value}`,
+        Subject : "Message from web work profile",
+        Body : `Personal Name: ${personalName.value}, Phone number: ${phoneNumber.value}, Message: ${message.value}`
+    }).then(
+        message => alert("e-mail sent successfully!"),
+        form.reset()
+    );
+});
